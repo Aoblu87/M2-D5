41023a199df7d1119737f7b351ca7025e9a3217b
Stefania <stefania.struzzi@gmail.com>
2023-07-03 21:10:55 +0200

    fatti gli esercizi con i numeri e calcoli

--- script.js
+++ script.js
@@ -1,52 +1,130 @@
-/* ESERCIZIO 1
+/* ESERCIZIO 1  FATTO
  Scrivi una funzione chiamata "crazySum" che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma di quei due valori, ma se il loro valore è lo stesso allora 
  deve ritornare la loro somma moltiplicata per 3.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// function crazySum(a, b) {
+//     if (a !== b) {
+//         let result = a + b
+//         return result
+//     } else {
+//         result = (a + b) * 3
+//         return result
 
-/* ESERCIZIO 2
+//     }
+// }
+// console.log(crazySum(5, 5))
+
+
+
+
+
+
+/* ESERCIZIO 2 FATTO
  Scrivi una funzione chiamata "boundary", che accetta un numero intero come parametro e ritorna true se tale 
  parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// function boundary(a) {
+//     if (a >= 20 && a <= 100 || a === 400)
+//         return true
+//     else {
+//         return false
+//     }
+// }
+// console.log(boundary(200))
+
+
+
 
 /* ESERCIZIO 3
- Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna 
+ Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna
  invertita (es.: EPICODE => EDOCIPE).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// function reverseString()
+
+
+
+
+
+
+
+
 
 /* ESERCIZIO 4
  Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo
   maiuscola ogni lettera iniziale di ogni parola.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+// function upperFirst(){
+//     if (a==="")
+//     a.toUpperCase()
+// }
+
+
+
+
 
 /* ESERCIZIO 5
- Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un 
+ Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un
  array contenente n numeri random contenuti tra 0 e 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// Math
+
+// function giveMeRandom(n){
+//     n= []
+//     for (let i = 0; i < n.length; i++) {
+//         const element = n[i];
+
+
+//     }
+
+
+// }
+
+
+
+
 
 //EXTRA:
-/* ESERCIZIO 1
+/* ESERCIZIO 1  FATTO
  Scrivi una funzione chiamata "area" che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// function area(l1, l2) {
+//     return l1 * l2
 
-/* ESERCIZIO 2
+// }
+
+// const areaRettengolo = area(5, 3)
+
+// console.log(areaRettengolo)
+
+
+
+/* ESERCIZIO 2 FATTO
  Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
  Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// function crazyDiff(a) {
+//     const num = 19
+//     let result = a - 19
+//     if (result >= 19) {
+//         return result * 3
+
+//     } return result
+// }
+// console.log(crazyDiff(30))
+
+
+
+
+
 
 /* ESERCIZIO 3
  Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
@@ -56,17 +134,32 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-/* ESERCIZIO 4
+
+
+
+
+/* ESERCIZIO 4 FATTO
  Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
  La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true;
   altrimenti deve tornare false.
  SUGGERIMENTO: operatore modulo
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+// function check3and7(a) {
+//     return (a % 3 === 0) || (a % 7 === 0)
+
+// }
+
+// console.log(check3and7(2))
+
+
+
+
+
 
 /* ESERCIZIO 5
- Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
+ Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro 
+ e la ritorna senza il primo e l'ultimo carattere.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
